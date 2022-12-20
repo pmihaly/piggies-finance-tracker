8c00980867aec098b015ddd10505501d59a1eb28
Mihaly Papp <misi@pappmihaly.com>
2022-12-20 13:04:33 +0100

    feat: add money

---
+++ src/Shared/ValueObjects/Money.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE EmptyDataDeriving #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+module Shared.ValueObjects.Money (Money (..), MoneyError, mkMoney) where
+
+import Control.Category ((>>>))
+import Test.QuickCheck (Arbitrary)
+
+newtype Money = UnsafeMoney {unMoney :: Double}
+  deriving newtype (Eq, Num, Fractional, Arbitrary)
+
+instance Show Money where
+  show = unMoney >>> show
+
+data MoneyError deriving (Eq, Show)
+
+mkMoney :: Double -> Either MoneyError Money
+mkMoney = UnsafeMoney >>> pure

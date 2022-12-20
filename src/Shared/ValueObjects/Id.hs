a92a943f1fc9f6a909f44ad2b9490339217a7c9c
Mihaly Papp <misi@pappmihaly.com>
2022-12-20 09:33:57 +0100

    feat: add id

---
+++ src/Shared/ValueObjects/Id.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+
+module Shared.ValueObjects.Id (Id (..), mkId, IdError (..)) where
+
+import Control.Category ((>>>))
+import Data.Bifunctor (bimap)
+import Data.Text qualified as T
+import Shared.ValueObjects.Text50 (Text50, Text50Error, mkText50)
+import Test.QuickCheck (Arbitrary)
+
+newtype Id = UnsafeId {unId :: Text50}
+  deriving newtype (Show, Eq, Arbitrary)
+
+newtype IdError
+  = IllegalId Text50Error
+  deriving (Show, Eq)
+
+mkId :: T.Text -> Either IdError Id
+mkId = mkText50 >>> bimap IllegalId UnsafeId

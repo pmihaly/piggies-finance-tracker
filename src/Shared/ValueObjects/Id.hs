be669b746d2eab0c02c333bd47a4128c64decff4
Mihaly Papp <misi@pappmihaly.com>
2022-12-21 19:19:56 +0100

    refactor: use phantom types for ids

--- src/Shared/ValueObjects/Id.hs
+++ src/Shared/ValueObjects/Id.hs
@@ -10,15 +10,15 @@ import Data.Text qualified as T
 import Shared.ValueObjects.Text50 (Text50, Text50Error, mkText50)
 import Test.QuickCheck (Arbitrary)
 
-newtype Id = UnsafeId {unId :: Text50}
+newtype Id a = UnsafeId {unId :: Text50}
   deriving newtype (Show, Eq, Arbitrary, ToJSON, IsString)
 
-instance FromJSON Id where
+instance FromJSON (Id a) where
   parseJSON = withText "Id" $ mkId >>> either (show >>> fail) pure
 
 newtype IdError
   = IllegalId Text50Error
   deriving (Show, Eq)
 
-mkId :: T.Text -> Either IdError Id
+mkId :: T.Text -> Either IdError (Id a)
 mkId = mkText50 >>> bimap IllegalId UnsafeId
--- test/Shared/ValueObjects/IdSpec.hs
+++ test/Shared/ValueObjects/IdSpec.hs
@@ -11,13 +11,13 @@ spec = do
   describe "introduction" $ do
     describe "parseJSON" $ do
       it "returns Nothing if input is empty" $
-        do (decode "\"\"" :: Maybe Id) `shouldBe` Nothing
+        do (decode "\"\"" :: Maybe (Id ())) `shouldBe` Nothing
 
       it "returns Nothing if the input is more than 500 characters" $
-        do (decode ("\"" <> TL.encodeUtf8 (TL.pack $ Prelude.take 501 ['A' ..]) <> "\"") :: Maybe Id) `shouldBe` Nothing
+        do (decode ("\"" <> TL.encodeUtf8 (TL.pack $ Prelude.take 501 ['A' ..]) <> "\"") :: Maybe (Id ())) `shouldBe` Nothing
 
       it "constructs the string with medium length input" $
-        do (decode "\"some-text\"" :: Maybe Id) `shouldBe` Just (UnsafeId "some-text")
+        do (decode "\"some-text\"" :: Maybe (Id ())) `shouldBe` Just (UnsafeId "some-text")
 
   describe "elimination" $ do
     it "can be converted to JSON using toJSON" $

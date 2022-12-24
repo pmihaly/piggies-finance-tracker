7b3078575c3a128669d7cd91773988696e0ca8fa
Mihaly Papp <misi@pappmihaly.com>
2022-12-24 16:49:01 +0100

    fix: add optionality to events and appliedEvents

--- src/Application/CLI/InputFile.hs
+++ src/Application/CLI/InputFile.hs
@@ -2,8 +2,9 @@
 
 module Application.CLI.InputFile (InputFile (..)) where
 
-import Data.Aeson (FromJSON (..), withObject, (.:))
+import Data.Aeson (FromJSON (..), withObject, (.!=), (.:), (.:?))
 import Data.HashMap.Strict qualified as Map
+import Data.HashSet qualified as Set
 import GHC.Generics (Generic)
 import PiggyBalance.Entities.Piggy (Piggy)
 import Shared.Entities.Event.Event (Event)
@@ -11,11 +12,14 @@ import Shared.ValueObjects.Id (Id)
 
 data InputFile = InputFile
   { piggyBalances :: Map.HashMap (Id Piggy) Piggy,
-    events :: [Event] }
+    events :: [Event],
+    appliedEvents :: Set.HashSet (Id Event)
+  }
   deriving (Show, Eq, Generic)
 
 instance FromJSON InputFile where
   parseJSON = withObject "InputFile" $ \obj -> do
     balances <- obj .: "piggy-balances"
-    events <- obj .: "events"
-    pure $ InputFile balances events
+    events <- obj .:? "events" .!= []
+    appliedEvents <- obj .:? "applied-events" .!= Set.empty
+    pure $ InputFile balances events appliedEvents

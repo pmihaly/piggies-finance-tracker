676204c4c5646ae3edecdb34a10d945b07c55e48
Mihaly Papp <misi@pappmihaly.com>
2022-12-27 12:29:05 +0100

    refactor: extract if-then-else of piggy lookup in piggy balances

--- src/PiggyBalance/OnEvent.hs
+++ src/PiggyBalance/OnEvent.hs
@@ -3,14 +3,18 @@
 module PiggyBalance.OnEvent (onEvent) where
 
 import Data.HashMap.Strict qualified as Map
-import PiggyBalance.Entities.Piggy (deposit)
+import PiggyBalance.Entities.Piggy (Piggy, deposit)
 import PiggyBalance.PiggyBalances (PiggyBalances)
 import PiggyBalance.ValueObjects.PiggyBalanceError (PiggyBalanceError (..))
 import Shared.Entities.Event.Event (Event (..))
+import Shared.ValueObjects.Id (Id)
 
 onEvent :: PiggyBalances -> Event -> Either PiggyBalanceError PiggyBalances
-onEvent balances (AddedToPiggy _ toPiggy amount) =
-  if Map.member toPiggy balances
-    then pure $ Map.adjust (deposit amount) toPiggy balances
-    else Left $ PiggyNotFound toPiggy
+onEvent balances (AddedToPiggy _ toPiggy amount) = ifExists toPiggy balances $ Map.adjust (deposit amount) toPiggy balances
 onEvent balances _ = pure balances
+
+ifExists :: Id Piggy -> PiggyBalances -> PiggyBalances -> Either PiggyBalanceError PiggyBalances
+ifExists toPiggy balances newBalances =
+  if Map.member toPiggy balances
+    then pure newBalances
+    else Left $ PiggyNotFound toPiggy

5bd77ffe0c6c73d5e500468b89bdf77bb457a54a
Mihaly Papp <misi@pappmihaly.com>
2022-12-28 17:01:06 +0100

    fix: add smart destructor to available money

--- src/Shared/ValueObjects/Available.hs
+++ src/Shared/ValueObjects/Available.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE DerivingVia #-}
 
-module Shared.ValueObjects.Available (Available, unAvailable, mkAvailableMoney, AvailableError (..), unsafeAvailable, arbitraryAvailableMoney) where
+module Shared.ValueObjects.Available (Available, unsafeUnAvailable, mkAvailableMoney, unmkAvailableMoney, AvailableError (..), unsafeAvailable, arbitraryAvailableMoney) where
 
 import Data.Aeson (FromJSON, ToJSON)
 import PiggyBalance.ValueObjects.Balance (Balance, unBalance)
@@ -10,7 +10,7 @@ import Shared.ValueObjects.NonZero (NonZero, unNonZero)
 import Shared.ValueObjects.Positive (Positive, unPositive)
 import Test.QuickCheck (Gen, arbitrary, suchThat)
 
-newtype Available a = Available {unAvailable :: a}
+newtype Available a = Available {unsafeUnAvailable :: a}
   deriving stock (Functor)
   deriving newtype (Show, Eq, Num, Fractional, FromJSON, ToJSON)
   deriving (RealFloat, RealFrac, Real, Floating, Ord) via a
@@ -26,6 +26,11 @@ mkAvailableMoney :: NonZero (Positive Money) -> Balance -> Either AvailableError
 mkAvailableMoney m b
   | unPositive (unNonZero m) > unBalance b = Left $ NotEnoughMoney $ unPositive $ unNonZero m
   | otherwise = Right $ unsafeAvailable m
+
+unmkAvailableMoney :: Available (NonZero (Positive Money)) -> Balance -> Either AvailableError (NonZero (Positive Money))
+unmkAvailableMoney (Available m) b
+  | unPositive (unNonZero m) > unBalance b = Left $ NotEnoughMoney $ unPositive $ unNonZero m
+  | otherwise = Right m
 
 arbitraryAvailableMoney :: Balance -> Gen (Available (NonZero (Positive Money)))
 arbitraryAvailableMoney b = unsafeAvailable <$> arbitrary `suchThat` (\m -> unPositive (unNonZero m) <= unBalance b)
--- test/Shared/ValueObjects/AvailableSpec.hs
+++ test/Shared/ValueObjects/AvailableSpec.hs
@@ -1,7 +1,7 @@
 module Shared.ValueObjects.AvailableSpec (spec) where
 
 import PiggyBalance.ValueObjects.Balance (unsafeBalance)
-import Shared.ValueObjects.Available (AvailableError (..), mkAvailableMoney, unsafeAvailable)
+import Shared.ValueObjects.Available (AvailableError (..), mkAvailableMoney, unmkAvailableMoney, unsafeAvailable)
 import Shared.ValueObjects.Money (unsafeMoney)
 import Shared.ValueObjects.NonZero (unNonZero, unsafeNonZero)
 import Shared.ValueObjects.Positive (unPositive, unsafePositive)
@@ -25,3 +25,21 @@ spec = do
           let expectedAvailableMoney = unsafeAvailable amount
 
           mkAvailableMoney amount balance `shouldBe` Right expectedAvailableMoney
+
+    describe "elimination" $ do
+      describe "unmkAvailableMoney" $ do
+        it "should return NotEnoughMoney on insufficient funds" $ do
+          let amount = unsafeNonZero $ unsafePositive $ unsafeMoney 1000.0
+          let availableAmount = unsafeAvailable amount
+          let balance = unsafeBalance $ unsafeMoney 500.0
+
+          let errorMessageMoney = unPositive $ unNonZero amount
+
+          unmkAvailableMoney availableAmount balance `shouldBe` Left (NotEnoughMoney errorMessageMoney)
+
+        it "should return an Available NonZero Positive Money on sufficient funds" $ do
+          let amount = unsafeNonZero $ unsafePositive $ unsafeMoney 1000.0
+          let availableAmount = unsafeAvailable amount
+          let balance = unsafeBalance $ unsafeMoney 1000.0
+
+          unmkAvailableMoney availableAmount balance `shouldBe` Right amount

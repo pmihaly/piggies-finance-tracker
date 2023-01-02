39061d4f709900cdd6f93d39879dd90e5e06de6d
Mihaly Papp <misi@pappmihaly.com>
2023-01-02 07:14:00 +0100

    refactor: remove unused and unsafe getOneId

--- src/PiggyBalance/PiggyBalances.hs
+++ src/PiggyBalance/PiggyBalances.hs
@@ -1,13 +1,9 @@
 {-# LANGUAGE ImportQualifiedPost #-}
 
-module PiggyBalance.PiggyBalances (PiggyBalances, getOneId) where
+module PiggyBalance.PiggyBalances (PiggyBalances) where
 
-import Control.Category ((>>>))
 import Data.HashMap.Strict qualified as Map
 import PiggyBalance.Entities.Piggy (Piggy)
 import Shared.ValueObjects.Id (Id)
 
 type PiggyBalances = Map.HashMap (Id Piggy) Piggy
-
-getOneId :: PiggyBalances -> Id Piggy
-getOneId = Map.toList >>> head >>> fst

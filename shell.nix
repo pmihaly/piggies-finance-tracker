45448e369ba69248de72b677480a0b5142e183d1
Mihaly Papp <misi@pappmihaly.com>
2023-02-12 10:07:00 +0100

    add some system-level dev-tools

--- shell.nix
+++ shell.nix
@@ -3,5 +3,18 @@
 pkgs.haskell.lib.buildStackProject {
   inherit ghc;
   name = "pigggies-finance-tracker-env";
-  buildInputs = with pkgs; [ zlib glpk pcre stack ];
+  buildInputs =
+  let
+    hPkgs = pkgs.haskell.packages."ghc924";
+  in
+    with hPkgs; [
+      stack
+      hlint
+      hoogle
+      retrie
+      # compilation fails with dies with error: cycle detected in build of '...' in the references of output 'bin' from output 'out'
+      # haskell-language-server
+      # ormolu
+      # ghcid
+    ];
 }
